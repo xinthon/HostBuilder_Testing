8a61b3ab5cdc81f7d997e007c659a21166f1633f
Sinthon <s.sinthon@cas.com.kh>
2023-02-18 10:28:57 +0700

    add user service

---
+++ HostBuilder_Testing/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using HostBuilder_Testing.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> Users();
+        Task<User> GetByUserId(Guid id);
+    }
+}
---
+++ HostBuilder_Testing/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HostBuilder_Testing.Data;
+using HostBuilder_Testing.Models;
+using HostBuilder_Testing.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HostBuilder_Testing.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly DataContext _dataContext;
+        public UserService(DataContext dataContext) 
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<User> GetByUserId(Guid userId)
+        {
+            return await _dataContext.Users.FindAsync(userId) ?? new();
+        }
+
+        public async Task<IEnumerable<User>> Users()
+        {
+            return await _dataContext.Users.ToListAsync() ?? new();
+        }
+    }
+}
