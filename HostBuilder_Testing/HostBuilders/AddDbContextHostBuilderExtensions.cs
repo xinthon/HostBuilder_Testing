2ed5ad3c3110fe1b403c49584c057389fb890b99
Sinthon <s.sinthon@cas.com.kh>
2023-02-18 10:27:40 +0700

    add db context host builder

---
+++ HostBuilder_Testing/HostBuilders/AddDbContextHostBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using HostBuilder_Testing.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.HostBuilders
+{
+    public static class AddDbContextHostBuilderExtensions
+    {
+        public static IHostBuilder AddDbContext(this IHostBuilder host)
+        {
+            host.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<DataContext>();
+            });
+
+            return host;
+        }
+    }
+}
