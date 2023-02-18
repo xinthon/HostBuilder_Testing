48d7b11af4c02a556b691e70cd0be9c0a808b111
Sinthon <s.sinthon@cas.com.kh>
2023-02-18 10:28:27 +0700

    add service host builder

---
+++ HostBuilder_Testing/HostBuilders/AddServicesHostBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using HostBuilder_Testing.Services;
+using HostBuilder_Testing.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.HostBuilders
+{
+    public static class AddServicesHostBuilderExtensions
+    {
+        public static IHostBuilder AddServices(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<IUserService, UserService>();
+            });
+
+            return host;
+        }
+    }
+}
