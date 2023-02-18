d9d47795341a0f202d7ccc3b4933ac6dce8e8247
Sinthon <s.sinthon@cas.com.kh>
2023-02-18 10:29:19 +0700

    add User Model

---
+++ HostBuilder_Testing/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
