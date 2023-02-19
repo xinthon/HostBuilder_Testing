3634dc8ad4aae247b90deef389b81b4f9a4b7569
Sinthon <s.sinthon@cas.com.kh>
2023-02-19 11:59:00 +0700

    add navigation state

---
+++ HostBuilder_Testing/State/Navigators/INavigator.cs
@@ -0,0 +1,20 @@
+﻿using HostBuilder_Testing.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.State.Navigators
+{
+    public enum ViewType
+    {
+        Home,
+    }
+
+    public interface INavigator
+    {
+        ViewModelBase CurrentViewModel { get; set; }
+        event Action StateChanged;
+    }
+}
---
+++ HostBuilder_Testing/State/Navigators/ViewModelDelegateRenavigator.cs
@@ -0,0 +1,26 @@
+﻿using HostBuilder_Testing.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.State.Navigators
+{
+    public class ViewModelDelegateRenavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    {
+        private readonly INavigator _navigator;
+        private readonly CreateViewModel<TViewModel> _createViewModel;
+
+        public ViewModelDelegateRenavigator(INavigator navigator, CreateViewModel<TViewModel> createViewModel)
+        {
+            _navigator = navigator;
+            _createViewModel = createViewModel;
+        }
+
+        public void Renavigate()
+        {
+            _navigator.CurrentViewModel = _createViewModel();
+        }
+    }
+}
