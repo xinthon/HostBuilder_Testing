c809d28f7733aa68154eb36605be38b1bc53c549
Sinthon <s.sinthon@cas.com.kh>
2023-02-19 11:57:41 +0700

    add view convter

---
+++ HostBuilder_Testing/Converters/EqualValueToParameterConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HostBuilder_Testing.Converters
+{
+    public class EqualValueToParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if(value == null)
+            {
+                return false;
+            }
+
+            return value.ToString() == parameter.ToString();;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
