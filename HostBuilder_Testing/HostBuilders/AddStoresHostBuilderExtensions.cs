3f72a2585301c98dde11d392d61709b1e5081322
Sinthon <s.sinthon@cas.com.kh>
2023-02-19 11:58:10 +0700

    add store host builder

---
+++ HostBuilder_Testing/HostBuilders/AddStoresHostBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using HostBuilder_Testing.State.Navigators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HostBuilder_Testing.HostBuilders
+{
+    public static class AddStoresHostBuilderExtensions
+    {
+        public static IHostBuilder AddStores(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<INavigator, Navigator>();
+            });
+
+            return host;
+        }
+    }
+}
