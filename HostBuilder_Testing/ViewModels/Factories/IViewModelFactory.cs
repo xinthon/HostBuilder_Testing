5c1fffafd2dd58b1558845bccdfa2bca91cb785b
Sinthon <s.sinthon@cas.com.kh>
2023-02-19 11:59:21 +0700

    add view factory

---
+++ HostBuilder_Testing/ViewModels/Factories/IViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using HostBuilder_Testing.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.ViewModels.Factories
+{
+    public interface ISimpleTraderViewModelFactory
+    {
+        ViewModelBase CreateViewModel(ViewType viewType);
+    }
+}
---
+++ HostBuilder_Testing/ViewModels/Factories/ViewModelFactory.cs
@@ -0,0 +1,32 @@
+﻿using HostBuilder_Testing.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostBuilder_Testing.ViewModels.Factories
+{
+    public class SimpleTraderViewModelFactory : ISimpleTraderViewModelFactory
+    {
+        private readonly CreateViewModel<HomeViewModel> _createHomeViewModel;
+
+        public SimpleTraderViewModelFactory(
+            CreateViewModel<HomeViewModel> createHomeViewModel
+            )
+        {
+            _createHomeViewModel = createHomeViewModel;
+        }
+
+        public ViewModelBase CreateViewModel(ViewType viewType)
+        {
+            switch (viewType)
+            {
+                case ViewType.Home:
+                    return _createHomeViewModel();
+                default:
+                    throw new ArgumentException("The ViewType does not have a ViewModel.", "viewType");
+            }
+        }
+    }
+}
