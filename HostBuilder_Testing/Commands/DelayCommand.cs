f1d07d96d6f1f4c8276b328c98cafdecaae2468b
Sinthon <s.sinthon@cas.com.kh>
2023-02-19 11:56:50 +0700

    add command

---
+++ HostBuilder_Testing/Commands/DelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HostBuilder_Testing.Commands
+{
+    public class DelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action<object> _action;
+
+        public DelayCommand(Action<object> action)
+        {
+            _action = action;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action?.Invoke(parameter);
+        }
+    }
+}
---
+++ HostBuilder_Testing/Commands/UpdateCurrentViewModelCommand.cs
@@ -0,0 +1,40 @@
+﻿using HostBuilder_Testing.State.Navigators;
+using HostBuilder_Testing.ViewModels.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HostBuilder_Testing.Commands
+{
+    public class UpdateCurrentViewModelCommand : ICommand
+    {
+        private readonly INavigator _navigator;
+        private readonly IViewModelFactory _viewModelFactory;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public UpdateCurrentViewModelCommand(INavigator navigator, IViewModelFactory viewModelFactory)
+        {
+            _navigator = navigator;
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is ViewType)
+            {
+                ViewType viewType = (ViewType)parameter;
+
+                _navigator.CurrentViewModel = _viewModelFactory.CreateViewModel(viewType);
+            }
+        }
+    }
+}
